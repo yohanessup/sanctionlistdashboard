6d8ed274d1ccf051cca60621554af6505768cd18
YohanesSuprapto <yohanes.suprapto@futuready.com>
2017-09-29 11:34:44 +0700

    Display username in header

--- application/views/home.php
+++ application/views/home.php
@@ -41,7 +41,7 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="<?php echo base_url('sanction/logout'); ?>">logout</a></li>
+                <li><a href="<?php echo base_url('sanction/logout'); ?>">logout (<?php echo $this->session->userdata('logged_in')['username']; ?>)</a></li>
             </ul>
             <!--<form class="navbar-form navbar-right">
                 <input type="text" class="form-control" placeholder="Search...">
